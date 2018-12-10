1390d92f64b51d244d6b4ecf4205b02d7eb7de2e
Mona Lisa <monalisa@alta3.com>
2018-12-10 18:44:21 -0500

    your commit message

---
+++ lab_input/input_ip.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# Written By: Alice Anderson
+# Collect IP address information from a user, and display it back to them.
+
+## Collect input from the user
+#user_input = input('Please enter an IPv4 IP address:')
+## Print out the input collected from the user
+#print("You told me the IPv4 address is:" + user_input)
+
+
+store_input = input('Please enter Vendor name:')
+
+print("List of Vendor Name:" + store_input)
